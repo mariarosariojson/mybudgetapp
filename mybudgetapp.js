3d879f770fac80e02b5aa0e17ab2518f235f4d72
Maria Rosario <mariarosariojohansson@gmail.com>
2022-05-04 16:13:23 +0200

    Budget app

--- mybudgetapp.js
+++ mybudgetapp.js
@@ -1,60 +1,53 @@
-document.querySelector("button")
-.addEventListener("click", countAmount);
-
 const listIn = [];
 const listOut = [];
+document.querySelector("#addToListBtn").addEventListener("click", countAmount);
 
 function countAmount(event) {
- event.preventDefault();
- //console.log("it is working!");
+  event.preventDefault();
+  //console.log("it is working!");
 
- let costSum = 0;
- let incomeSum = 0;
+  let costSum = 0;
+  let incomeSum = 0;
 
- const select = document.querySelector("select");
- // console.log(select.value);
+  const select = document.querySelector("select");
+  // console.log(select.value);
 
-    const description = document.querySelector(".description").value;
-    // console.log(description);
+  const description = document.querySelector(".description").value;
+  // console.log(description);
 
-    const num = document.querySelector(".num").value;
-    // console.log(num);
+  const num = document.querySelector(".num").value;
+  // console.log(num);
 
-    if(select.value=="+") {
-        document
-        .querySelector(".listIn")
-        .innerHTML += `<li> ${description}: ${num}SEK</li>`;
-        const divIn = document.querySelector(".listIn");
+  if (select.value == "+") {
+    document.querySelector(
+      ".listIn"
+    ).innerHTML += `<li> ${description}: ${num}SEK</li>`;
+    const divIn = document.querySelector(".listIn");
 
-        listIn.push(num);
-        //console.log(listIn);
-        
-    }
+    listIn.push(num);
+    //console.log(listIn);
+  } else if (select.value == "-") {
+    document.querySelector(
+      ".listOut"
+    ).innerHTML += `<li> ${description}: ${num}SEK</li>`;
 
-    else if(select.value=="-") {
-        document
-        .querySelector(".listOut")
-        .innerHTML += `<li> ${description}: ${num}SEK</li>`;
+    listOut.push(num);
+    // console.log(listOut);
+  } else if (select.value == "choose") {
+    alert("Please, type in + or -");
+  }
 
-        listOut.push(num);
-        // console.log(listOut);
-    }
+  listIn.map((e) => {
+    console.log(parseFloat(e));
+    incomeSum += parseFloat(e);
+  });
 
-    else if(select.value=="choose") {
-        alert("Please, type in + or -");
-    }
+  listOut.map((e) => {
+    console.log(parseFloat(e));
+    costSum += parseFloat(e);
+  });
 
-    listIn.map((e) => {
-        console.log(parseFloat(e));
-        incomeSum += parseFloat(e);
-    })
-
-    listOut.map((e) => {
-        console.log(parseFloat(e));
-        costSum += parseFloat(e);
-    })
-
-    document.querySelector(".out").innerHTML = costSum;
-    document.querySelector(".budget").innerHTML = incomeSum - costSum;
-    document.querySelector(".in").innerHTML = incomeSum;
+  document.querySelector(".out").innerHTML = costSum;
+  document.querySelector(".budget").innerHTML = incomeSum - costSum;
+  document.querySelector(".in").innerHTML = incomeSum;
 }
